db8fcd2cda188df894c1ea4aed2c1c831cd19a1f
Balekage Gael <gbalekage21@gmail.com>
2025-12-29 07:31:13 +0100

    121

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -203,15 +203,15 @@ const login = async (req, res, next) => {
 
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: stayLoggedIn ? 30 * 24 * 60 * 60 * 1000 : 15 * 60 * 1000,
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: stayLoggedIn ? 30 * 24 * 60 * 60 * 1000 : 7 * 24 * 60 * 60 * 1000,
     });
 
