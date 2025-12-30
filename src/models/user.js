e400ec7a35e928adea091740ca64aeb736544ea7
Balekage Gael <gbalekage21@gmail.com>
2025-12-30 22:25:29 +0100

    1212

--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,38 @@
+const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    googleId: { type: String, unique: true, sparse: true }, // <--- add this
     role: { type: String, enum: ["admin", "student"], default: "student" },
     password: { type: String, required: true },
     isActive: { type: Boolean, default: true },
+    googleId: { type: String, unique: true, sparse: true },
+
     phone: { type: String },
     phoneCode: { type: String },
     phoneCodeExpiration: { type: Date },
     isPhoneVerified: { type: Boolean, default: false },
+
     emailCode: { type: String },
     emailCodeExpiration: { type: Date },
     isEmailVerified: { type: Boolean, default: false },
+
     pendingEmail: { type: String },
     pendingEmailCode: { type: String },
     pendingEmailCodeExpiration: { type: Date },
+
     resetPasswordToken: { type: String },
     resetPasswordTokenExpiration: { type: Date },
+
     refreshToken: { type: String },
-    province: { type: mongoose.Schema.Types.ObjectId, ref: "Province" },
+    province: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Province",
+    },
     section: { type: mongoose.Schema.Types.ObjectId, ref: "Section" },
     subscription: { type: mongoose.Schema.Types.ObjectId, ref: "Subscription" },
+
     stats: {
       coursesCompleted: Number,
       averageScore: Number,
@@ -30,3 +41,5 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+module.exports = mongoose.model("User", userSchema);
